bbea0f5440c4258edfed4ff2704814388f501996
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-05-17 17:30:26 +0200

    Acabado

--- mordor/src/main/java/edu/pingpong/mordor/procesadores/Oficina.java
+++ mordor/src/main/java/edu/pingpong/mordor/procesadores/Oficina.java
@@ -3,6 +3,7 @@ package edu.pingpong.mordor.procesadores;
 import edu.pingpong.mordor.interfaces.Pedido;
 import edu.pingpong.mordor.interfaces.Procesador;
 import edu.pingpong.mordor.interfaces.TratamientoPedido;
+import edu.pingpong.mordor.interfaces.Status;
 
 public class Oficina implements Procesador {
 
@@ -14,7 +15,9 @@ public class Oficina implements Procesador {
     }
 
     public String printarStatus(boolean admitido, Pedido pedido) {
-        return ""; //Falta implementar lógica
+        return admitido? 
+				pedido.destino() + " " + Status.ACEPTADO.name():
+				pedido.destino() + " " + Status.RECHAZADO.name();
     }
 
 }
